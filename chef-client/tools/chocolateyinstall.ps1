c619f7729202466063f04c847993d079d58ff278
Tim Smith <tsmith@chef.io>
2018-09-04 09:33:00 -0700

    Add chef-client 14.4.56
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chef-client/tools/chocolateyinstall.ps1
+++ chef-client/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $packageArgs = @{
   packageName = 'chef-client'
   fileType = 'msi'
-  url = 'https://packages.chef.io/files/stable/chef/14.2.0/windows/2016/chef-client-14.2.0-1-x86.msi'
-  url64 = 'https://packages.chef.io/files/stable/chef/14.2.0/windows/2016/chef-client-14.2.0-1-x64.msi'
+  url = 'https://packages.chef.io/files/stable/chef/14.4.56/windows/2016/chef-client-14.4.56-1-x86.msi'
+  url64 = 'https://packages.chef.io/files/stable/chef/14.4.56/windows/2016/chef-client-14.4.56-1-x64.msi'
   silentArgs = '/quiet'
   validExitCodes = @(0)
-  checksum = '881933ef41d879946efb080459b87b1f1f6bec86eae895e7f229d4778f398b7d'
+  checksum = '68eef14518e0dcdc7c5eae8c26f4a4141b94183f55157d4962553f4c3b67bfb3'
   checksumType = 'sha256'
-  checksum64 = '8744c716386b82f792f41ba286431e88afeb3efd68ee46cd46516b1d6930722c'
+  checksum64 = '1d6c200142359c27d799477e9ef546a455df0fb04a30676142117ddf9cc38bb4'
   checksumType64 = 'sha256'
 }
 
