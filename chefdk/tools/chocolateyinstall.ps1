04c25d4284e702a3b173715fdf77deccf6ce139e
Tim Smith <tsmith@chef.io>
2018-11-01 16:49:07 -0700

    Update DK to 3.4
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chefdk/tools/chocolateyinstall.ps1
+++ chefdk/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '3.3.23'
+$version = '3.4.38'
 $url = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x86.msi"
 $url64 = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x64.msi"
 
@@ -8,9 +8,9 @@ $packageArgs = @{
     fileType      = 'MSI'
     url           = $url
     url64bit      = $url64
-    checksum      = 'f6b0f599ed09b21a1e4888831345a17fc174fd9ce1c61bd68ac8d11d9fc4a2d1'
+    checksum      = 'f21504310024b0ac31802065c3e9fa45fe0f60f66518375c9890aa11f5f609cc'
     checksumType  = 'sha256'
-    checksum64    = 'a47042cfdb792d7e87ff5c7a61e24aedd56f21b80bf93bd64371b933bb069638'
+    checksum64    = 'cd8c1d306b3a0e7f5f75328b8d9b0643f12c2492352c2e911dc247bce66c77d9'
     checksumType64= 'sha256'
     silentArgs    = "/qn /quiet /norestart"
     validExitCodes= @(0, 3010)
