537c745fd8e2c6ab6477ef869c1b9bdb1c6f7467
Tim Smith <tsmith@chef.io>
2018-11-12 10:45:43 -0800

    Update chef-client to 14.7
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chef-client/tools/chocolateyinstall.ps1
+++ chef-client/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $packageArgs = @{
   packageName = 'chef-client'
   fileType = 'msi'
-  url = 'https://packages.chef.io/files/stable/chef/14.6.47/windows/2016/chef-client-14.6.47-1-x86.msi'
-  url64 = 'https://packages.chef.io/files/stable/chef/14.6.47/windows/2016/chef-client-14.6.47-1-x64.msi'
+  url = 'https://packages.chef.io/files/stable/chef/14.7.17/windows/2016/chef-client-14.7.17-1-x86.msi'
+  url64 = 'https://packages.chef.io/files/stable/chef/14.7.17/windows/2016/chef-client-14.7.17-1-x64.msi'
   silentArgs = '/quiet'
   validExitCodes = @(0)
-  checksum = '1690d453c4e94742f18c47140c160a69609da34bca681fca6bc690649ffcf326'
+  checksum = 'fb114dea5f5451bd7e24c2930ec8913271fe90da401ffcddded4d33f6615c0de'
   checksumType = 'sha256'
-  checksum64 = '5d20a33973472d64c40ff4642a94702c2c2205174f8178123199f40931d8d774'
+  checksum64 = '17e570f1bce16d3bb090addbbfdb97fb78fe6d96da88ff20c456c5e2567828d4'
   checksumType64 = 'sha256'
 }
 
