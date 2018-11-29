b1b7ff51cc4f123b5f85455a493d00dcf4da05d4
Tim Smith <tsmith@chef.io>
2018-11-29 10:11:45 -0800

    Update ChefDK and Chef Workstation to the latest
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chef-workstation/tools/chocolateyinstall.ps1
+++ chef-workstation/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '0.2.29'
+$version = '0.2.39'
 $url64 = "https://packages.chef.io/files/stable/chef-workstation/$version/windows/2016/chef-workstation-$version-1-x64.msi"
 
 $packageArgs = @{
@@ -6,7 +6,7 @@ $packageArgs = @{
     unzipLocation = $toolsDir
     fileType      = 'MSI'
     url64bit      = $url64
-    checksum64    = 'a72e453d8caa9c3284b3c308bbbab09527142f805ff9533b7cbbea13aaf170a4'
+    checksum64    = '9d122ff22bf0a508a48bbf56d51d484739a01eda45e8b7560ec10d0c573bf0f1'
     checksumType64= 'sha256'
     silentArgs    = "/qn /quiet /norestart"
     validExitCodes= @(0, 3010)
--- chefdk/tools/chocolateyinstall.ps1
+++ chefdk/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '3.4.38'
+$version = '3.5.13'
 $url = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x86.msi"
 $url64 = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x64.msi"
 
@@ -8,9 +8,9 @@ $packageArgs = @{
     fileType      = 'MSI'
     url           = $url
     url64bit      = $url64
-    checksum      = 'f21504310024b0ac31802065c3e9fa45fe0f60f66518375c9890aa11f5f609cc'
+    checksum      = 'f02d756e04958ea050008432e03f26fae3c363e597e51391b15931254e2eee8e'
     checksumType  = 'sha256'
-    checksum64    = 'cd8c1d306b3a0e7f5f75328b8d9b0643f12c2492352c2e911dc247bce66c77d9'
+    checksum64    = '8007bf40f06e11ce0957176e63d834f19d4f498768ea90ab8053ec659c2ed748'
     checksumType64= 'sha256'
     silentArgs    = "/qn /quiet /norestart"
     validExitCodes= @(0, 3010)
