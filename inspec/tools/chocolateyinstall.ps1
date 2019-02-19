a17ee31ded008bf0791300bb2aea9402430472c5
Tim Smith <tsmith@chef.io>
2019-02-19 12:20:50 -0800

    Add InSpec package
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ inspec/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+$version = '3.6.6'
+$url = "https://packages.chef.io/stable/windows/2012/inspec-$version-1-x64.msi"
+
+$packageArgs = @{
+    packageName    = 'inspec'
+    unzipLocation  = $toolsDir
+    fileType       = 'MSI'
+    url64bit       = $url
+    checksum64     = '391e8d62436820f07a9ac89d4ab163703a3541f1d33ff8b76d18aea35d3d3b7c'
+    checksumType64 = 'sha256'
+    silentArgs     = "/qn /quiet /norestart"
+    validExitCodes = @(0, 3010)
+}
+
+Install-ChocolateyPackage @packageArgs
