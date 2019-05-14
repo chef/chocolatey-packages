385261d29eb1e1ec630f4c263899f14174e4dffb
Tim Smith <tsmith@chef.io>
2019-05-14 15:54:24 -0700

    Update chef-client to 14.11.21
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chef-client/tools/chocolateyinstall.ps1
+++ chef-client/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '14.10.9'
+$version = '14.11.21'
 
 $packageArgs = @{
     packageName    = 'chef-client'
@@ -7,9 +7,9 @@ $packageArgs = @{
     url64          = "https://packages.chef.io/files/stable/chef/$version/windows/2016/chef-client-$version-1-x64.msi"
     silentArgs     = '/quiet'
     validExitCodes = @(0)
-    checksum       = 'f1daaf3d0bb8c6c7bbb54e201febcc4269f06565dfbecf64c375acff0dca01ed'
+    checksum       = '8147c97cd3e551f3a5b9879cc86693db4382b8c9ee4eb9e48ecb534667611120'
     checksumType   = 'sha256'
-    checksum64     = 'e975ed135bd56c54019ed0115bface82c3cf7c0067a7e81c64cfc42db5e40561'
+    checksum64     = 'a7519c4e4450ab7cd7ea545283a13c30ed0a12a140260beef4ce8d9a86883077'
     checksumType64 = 'sha256'
 }
 
