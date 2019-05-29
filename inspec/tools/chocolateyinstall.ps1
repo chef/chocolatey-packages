64dde1acc73364ea21023ccfc79ca62e98411bab
Tim Smith <tsmith@chef.io>
2019-05-28 17:27:14 -0700

    Update InSpec to 4.3.2
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- inspec/tools/chocolateyinstall.ps1
+++ inspec/tools/chocolateyinstall.ps1
@@ -1,11 +1,10 @@
-$version = '3.9.3'
-$url = "https://packages.chef.io/stable/windows/2012/inspec-$version-1-x64.msi"
+$version = '4.3.2'
 
 $packageArgs = @{
     packageName    = 'inspec'
     unzipLocation  = $toolsDir
-    fileType       = 'MSI'
-    url64bit       = $url
+    fileType       = 'msi'
+    url64bit       = "https://packages.chef.io/files/stable/inspec/$version/windows/2016/inspec-$version-1-x64.msi"
     checksum64     = '391e8d62436820f07a9ac89d4ab163703a3541f1d33ff8b76d18aea35d3d3b7c'
     checksumType64 = 'sha256'
     silentArgs     = "/qn /quiet /norestart"
