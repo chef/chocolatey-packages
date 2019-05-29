8eecdd05f142e6c60ae89b7775c47cc8454a5ca1
Tim Smith <tsmith@chef.io>
2019-05-28 17:15:50 -0700

    Update Chef Workstation to 0.2.53
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chef-workstation/tools/chocolateyinstall.ps1
+++ chef-workstation/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '0.2.48'
+$version = '0.2.53'
 $url64 = "https://packages.chef.io/files/stable/chef-workstation/$version/windows/2016/chef-workstation-$version-1-x64.msi"
 
 $packageArgs = @{
@@ -6,7 +6,7 @@ $packageArgs = @{
     unzipLocation  = $toolsDir
     fileType       = 'MSI'
     url64bit       = $url64
-    checksum64     = '1e5245bfad464ac03162d8b8f1d2688f03be983669ea22f1c6c38249a737d4fe'
+    checksum64     = '10cf78a487d2010336ecde920aeec98b1f8c088df21f8f45babd3937d269d455'
     checksumType64 = 'sha256'
     silentArgs     = "/qn /quiet /norestart"
     validExitCodes = @(0, 3010)
