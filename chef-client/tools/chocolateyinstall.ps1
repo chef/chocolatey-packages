4c81ba4534c487561d199face0ee7fde9ab53628
Tim Smith <tsmith@chef.io>
2019-05-28 17:06:15 -0700

    Update chef-client to 15.0.300
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chef-client/tools/chocolateyinstall.ps1
+++ chef-client/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '15.0.293'
+$version = '15.0.300'
 
 $packageArgs = @{
     packageName    = 'chef-client'
@@ -7,9 +7,9 @@ $packageArgs = @{
     url64          = "https://packages.chef.io/files/stable/chef/$version/windows/2016/chef-client-$version-1-x64.msi"
     silentArgs     = '/quiet'
     validExitCodes = @(0)
-    checksum       = 'c22b5759fb16415c283ac4ec50e3b33f9ced0e7ebb941d8e4d2b71a4a3addef6'
+    checksum       = '279f3cfa40e3455d488e1085d93464a7046e6ea3dc258d12bb206d8fbfc6eb0d'
     checksumType   = 'sha256'
-    checksum64     = 'e66a0b6aa855538edca50cce89a21a80a6e52e78f5cf15b944ef6e131c1f561b'
+    checksum64     = 'eb84a65bd50474c2e864b676cb5c91a5336d8bddf77eec944ce6db7f8e2c1f7e'
     checksumType64 = 'sha256'
 }
 
