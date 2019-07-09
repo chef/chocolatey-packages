57833109ed33609f65f3e40d7f40bc2d0d21da8a
Tim Smith <tsmith84@gmail.com>
2019-07-09 15:57:43 -0700

    update DK to 4.1.7
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chefdk/tools/chocolateyinstall.ps1
+++ chefdk/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '4.0.60'
+$version = '4.1.7'
 $url = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x86.msi"
 $url64 = "https://packages.chef.io/stable/windows/2012r2/chefdk-$version-1-x64.msi"
 
@@ -8,9 +8,9 @@ $packageArgs = @{
     fileType       = 'MSI'
     url            = $url
     url64bit       = $url64
-    checksum       = '09a423bbf690af7abaa08d5a1dddd38bc399316e8506e6734e1e91a8d959677c'
+    checksum       = 'de4cd400b534d9e6888278de1473b31b39aadb58593bed8b6edcbf4beecf2876'
     checksumType   = 'sha256'
-    checksum64     = '80d3a65ad9b681b7125b8aed3696a222a782ccceac8284097ba6366c4ef978ab'
+    checksum64     = '5af726b6ffe0d6da0e66f327268d73e62d4bacd8f468e45caffad0bad160ac58'
     checksumType64 = 'sha256'
     silentArgs     = "/qn /quiet /norestart"
     validExitCodes = @(0, 3010)
