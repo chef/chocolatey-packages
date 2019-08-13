270d2afeabf46b66708a25e0de879d76af7583aa
Tim Smith <tsmith84@gmail.com>
2019-08-12 17:20:12 -0700

    Update Chef InSpec to 4.11.3
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- inspec/tools/chocolateyinstall.ps1
+++ inspec/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-$version = '4.7.3'
+$version = '4.11.3'
 
 $packageArgs = @{
     packageName    = 'inspec'
     unzipLocation  = $toolsDir
     fileType       = 'msi'
     url64bit       = "https://packages.chef.io/files/stable/inspec/$version/windows/2016/inspec-$version-1-x64.msi"
-    checksum64     = 'a90181255a2a588985cb12aa3c87e59ac778ed0a5e99cdff475458d34462ae44'
+    checksum64     = '777c386d8773d31b457d9b6b419fe951fae030f0d13d896532623a58cd58bf9d'
     checksumType64 = 'sha256'
     silentArgs     = "/qn /quiet /norestart"
     validExitCodes = @(0, 3010)
