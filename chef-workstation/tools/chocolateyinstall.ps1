6d5edb8f48ee349ad0ae63331754258cd0e16183
Tim Smith <tsmith84@gmail.com>
2019-08-12 17:17:45 -0700

    Update Chef Workstation to 0.8.7
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chef-workstation/tools/chocolateyinstall.ps1
+++ chef-workstation/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '0.7.4'
+$version = '0.8.7'
 $url64 = "https://packages.chef.io/files/stable/chef-workstation/$version/windows/2016/chef-workstation-$version-1-x64.msi"
 
 $packageArgs = @{
@@ -6,7 +6,7 @@ $packageArgs = @{
     unzipLocation  = $toolsDir
     fileType       = 'MSI'
     url64bit       = $url64
-    checksum64     = 'c6a671a4ef608f8c7e1e51bbea4b4a6ae39b29dbe5a2415e3fc744ad77f0cf30'
+    checksum64     = '578623d3507cb54658103e71c53f525489669524d87ffec913805245feb6fa1a'
     checksumType64 = 'sha256'
     silentArgs     = "/qn /quiet /norestart"
     validExitCodes = @(0, 3010)
