97fd3d95241a643dd0b2ba10189cbc354b35799a
Tim Smith <tsmith84@gmail.com>
2019-11-04 09:26:28 -0800

    Update Workstation to 0.11.21
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chef-workstation/tools/chocolateyinstall.ps1
+++ chef-workstation/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '0.10.41'
+$version = '0.11.21'
 $url64 = "https://packages.chef.io/files/stable/chef-workstation/$version/windows/2016/chef-workstation-$version-1-x64.msi"
 
 $packageArgs = @{
@@ -6,7 +6,7 @@ $packageArgs = @{
     unzipLocation  = $toolsDir
     fileType       = 'MSI'
     url64bit       = $url64
-    checksum64     = 'a304a82cf8d41974158f6abc0bc76a0ecb7316635938220aeaf41f1729c01f5b'
+    checksum64     = 'ea2986fe5f650cf615834d2ad2247bee506a03e3c8849af5a28b2ccbd6b76bea'
     checksumType64 = 'sha256'
     silentArgs     = "/qn /quiet /norestart"
     validExitCodes = @(0, 3010)
