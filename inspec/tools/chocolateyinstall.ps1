eb40ee2dbb33d70b470f798bce0a6bb7a41ca122
Tim Smith <tsmith84@gmail.com>
2019-11-04 09:31:05 -0800

    Update InSpec to 4.18.24
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- inspec/tools/chocolateyinstall.ps1
+++ inspec/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-$version = '4.18.0'
+$version = '4.18.24'
 
 $packageArgs = @{
     packageName    = 'inspec'
     unzipLocation  = $toolsDir
     fileType       = 'msi'
     url64bit       = "https://packages.chef.io/files/stable/inspec/$version/windows/2016/inspec-$version-1-x64.msi"
-    checksum64     = '45de89e7a96984e587c24c38458d7d57f824ac222f3c7a75b1ae698cc483e563'
+    checksum64     = '59f652afc355201ccd4395e57f6bbf58b40403e11c17aad1baeed24e27edeb64'
     checksumType64 = 'sha256'
     silentArgs     = "/qn /quiet /norestart"
     validExitCodes = @(0, 3010)
