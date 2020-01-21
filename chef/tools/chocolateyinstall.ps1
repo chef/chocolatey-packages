3f4588d9df3410d83847aafc6c11a85a7ad48c9a
Gusztáv Varga <mail@gusztavvargadr.me>
2020-01-21 20:46:38 +0100

    Update Chef to version 15.6.10
    Signed-off-by: Gusztáv Varga <mail@gusztavvargadr.me>

--- chef/tools/chocolateyinstall.ps1
+++ chef/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '15.5.17'
+$version = '15.6.10'
 
 $packageArgs = @{
     packageName    = 'chef-client'
@@ -7,9 +7,9 @@ $packageArgs = @{
     url64          = "https://packages.chef.io/files/stable/chef/$version/windows/2012r2/chef-client-$version-1-x64.msi"
     silentArgs     = '/quiet'
     validExitCodes = @(0)
-    checksum       = '3b1aea92f6596e75e2b244dfdf88d5045ef20ff326a86667025c81057c8a67f6'
+    checksum       = '551171d2f83b8926d62f173098914de81291fff8bf7ab61336e4cff0bcb5baaf'
     checksumType   = 'sha256'
-    checksum64     = 'e728a43fcadcc0fa7979c879ad6aa692a53492d93e8cc0c6752d6d0bd030a7fd'
+    checksum64     = '5553cea94fc5c374403751324619802509b906ef8e2b92a36a5fc71b35dbb1e2'
     checksumType64 = 'sha256'
 }
 
