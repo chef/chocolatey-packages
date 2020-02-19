463d6380755f2dbc681726ff0045f94e585405d6
Tim Smith <tsmith84@gmail.com>
2020-02-19 09:10:22 -0800

    Update Chef Infra Client to 15.8.23
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chef/tools/chocolateyinstall.ps1
+++ chef/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '15.7.32'
+$version = '15.8.23'
 
 $packageArgs = @{
     packageName    = 'chef-client'
@@ -7,9 +7,9 @@ $packageArgs = @{
     url64          = "https://packages.chef.io/files/stable/chef/$version/windows/2012r2/chef-client-$version-1-x64.msi"
     silentArgs     = '/quiet'
     validExitCodes = @(0)
-    checksum       = 'e0fefdd95cc8950f6fb2e186f6011541af0f9bc8a2de0310a045ff0299bf3411'
+    checksum       = '7933199932ff103450374112638e947b1f9f12cc697de244bc8b0ed6b42f2257'
     checksumType   = 'sha256'
-    checksum64     = 'b4bf68b6f9e55115b66d2a666f2e8d026ead8792ef935ec58e48cb56c3924b67'
+    checksum64     = '885c5d139c781ee1a347fe5905d9133bc6e061e5cb49be3c744315c05bb57185'
     checksumType64 = 'sha256'
 }
 
