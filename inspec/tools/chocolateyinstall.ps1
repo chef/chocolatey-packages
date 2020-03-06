f70814143995ce1272d774d197f9bafb55a4a224
Daniel Sutton <daniel@ducksecops.uk>
2020-03-06 13:28:28 +0000

    Updated Inspec to 4.18.100

--- inspec/tools/chocolateyinstall.ps1
+++ inspec/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-$version = '4.18.51'
+$version = '4.18.100'
 
 $packageArgs = @{
     packageName    = 'inspec'
     unzipLocation  = $toolsDir
     fileType       = 'msi'
     url64bit       = "https://packages.chef.io/files/stable/inspec/$version/windows/2016/inspec-$version-1-x64.msi"
-    checksum64     = '2db71f31f4d603c011cb9c08b701c7a2040d2fb2597c615bdab3937e5d1e2b36'
+    checksum64     = '270b01869085d87e0d7726895099c5290ad106fa88840bfff3679a08d5a7884e'
     checksumType64 = 'sha256'
     silentArgs     = "/qn /quiet /norestart"
     validExitCodes = @(0, 3010)
