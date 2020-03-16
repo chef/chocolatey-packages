05f47fe4b8b62f0949fa88bd0dcf6d9f9e7335d6
Daniel Sutton <daniel@ducksecops.uk>
2020-03-16 10:27:46 +0000

    Updated Chef Workstation 0.17.5
    Signed-off-by: Daniel Sutton <daniel@ducksecops.uk>

--- chef-workstation/tools/chocolateyinstall.ps1
+++ chef-workstation/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '0.16.33'
+$version = '0.17.5'
 $url64 = "https://packages.chef.io/files/stable/chef-workstation/$version/windows/2016/chef-workstation-$version-1-x64.msi"
 
 $packageArgs = @{
@@ -6,7 +6,7 @@ $packageArgs = @{
     unzipLocation  = $toolsDir
     fileType       = 'MSI'
     url64bit       = $url64
-    checksum64     = '8556fab7e35cc14f67bc78867722b165adfd1403bdba7314cc894509c77afebd'
+    checksum64     = 'cb59b4721c176a64c211bc3a0f5accfdd06bd6d26b816d0406b2c82464ab4555'
     checksumType64 = 'sha256'
     silentArgs     = "/qn /quiet /norestart"
     validExitCodes = @(0, 3010)
