e8659580ba72f46c70e5961b837bef293ef1870f
Tim Smith <tsmith84@gmail.com>
2020-04-09 11:10:12 -0700

    Update Chef Infra Client and InSpec to the latest
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chef/tools/chocolateyinstall.ps1
+++ chef/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '15.8.23'
+$version = '15.9.17'
 
 $packageArgs = @{
     packageName    = 'chef-client'
@@ -7,9 +7,9 @@ $packageArgs = @{
     url64          = "https://packages.chef.io/files/stable/chef/$version/windows/2012r2/chef-client-$version-1-x64.msi"
     silentArgs     = '/quiet'
     validExitCodes = @(0)
-    checksum       = '7933199932ff103450374112638e947b1f9f12cc697de244bc8b0ed6b42f2257'
+    checksum       = '65758e9ddd9cf87ad9a9e89f7c543b661eb1a5e61b2a55c5cccc69643b2e4eb6'
     checksumType   = 'sha256'
-    checksum64     = '885c5d139c781ee1a347fe5905d9133bc6e061e5cb49be3c744315c05bb57185'
+    checksum64     = '44b76b17d9f18cceaa30309d5e636fdd547ac7725f9a6700ac2e54c81d6ac92a'
     checksumType64 = 'sha256'
 }
 
--- inspec/tools/chocolateyinstall.ps1
+++ inspec/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-$version = '4.18.100'
+$version = '4.18.104'
 
 $packageArgs = @{
     packageName    = 'inspec'
     unzipLocation  = $toolsDir
     fileType       = 'msi'
     url64bit       = "https://packages.chef.io/files/stable/inspec/$version/windows/2016/inspec-$version-1-x64.msi"
-    checksum64     = '270b01869085d87e0d7726895099c5290ad106fa88840bfff3679a08d5a7884e'
+    checksum64     = '753093ac1f16c01fe4e9470fdc7083d2225877d2c445561f12405483a9368632'
     checksumType64 = 'sha256'
     silentArgs     = "/qn /quiet /norestart"
     validExitCodes = @(0, 3010)
