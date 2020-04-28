910aa27cb1472a1cd367ae2c696dc564ccf65253
Tim Smith <tsmith84@gmail.com>
2020-04-28 14:19:29 -0700

    Add Chef Infra Client 16.0.257
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chef/tools/chocolateyinstall.ps1
+++ chef/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '15.10.12'
+$version = '16.0.257'
 
 $packageArgs = @{
     packageName    = 'chef-client'
@@ -7,9 +7,9 @@ $packageArgs = @{
     url64          = "https://packages.chef.io/files/stable/chef/$version/windows/2012r2/chef-client-$version-1-x64.msi"
     silentArgs     = '/quiet'
     validExitCodes = @(0)
-    checksum       = '0f56bcd8afb753e02bc77619b4b552f01ce5991566a8ca727bd06bd7e4992b80'
+    checksum       = '71c6c4fa4dce7b4c905ae1b853d79d4dd39c72ae7cb9b09a61fb2dd196c405c7'
     checksumType   = 'sha256'
-    checksum64     = '3173a73a6fe1073868bb258cf66b2a5ca78f94974f04bd581fe6ea8b32acaccf'
+    checksum64     = '7be69163ac793ac31e4f4a447164fb90790599617ee5ad4cc6e3aa3fbb46da7f'
     checksumType64 = 'sha256'
 }
 
