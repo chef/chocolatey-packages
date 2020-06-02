bda560667c246d516b7846893012e45f075a1254
Tim Smith <tsmith84@gmail.com>
2020-06-02 13:49:59 -0700

    Update Chef Infra Client to 16.1.6
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chef/tools/chocolateyinstall.ps1
+++ chef/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '16.1.0'
+$version = '16.1.6'
 
 $packageArgs = @{
     packageName    = 'chef-client'
@@ -7,9 +7,9 @@ $packageArgs = @{
     url64          = "https://packages.chef.io/files/stable/chef/$version/windows/2012r2/chef-client-$version-1-x64.msi"
     silentArgs     = '/quiet'
     validExitCodes = @(0)
-    checksum       = '48f52a1bf4cd75f0ebcc1035efe71f845734bdf9038c1e717be6e9c6e60f82e7'
+    checksum       = '68d7b0020bdc90c4af52cacd82c7e219f93898e54078ef78505a036112fe3e5c'
     checksumType   = 'sha256'
-    checksum64     = '1ddbf313f81c345f98cacecc08347ced8ab477d4786217b84a2ae6011b7de6c5'
+    checksum64     = '19be54232bbf7503af9c236944b2fd6d72089be370f30e74eed7f6e1ef396cd4'
     checksumType64 = 'sha256'
 }
 
