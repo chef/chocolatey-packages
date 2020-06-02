4a055eafac2d6cd6edf7cb0eced0142f168dd621
Tim Smith <tsmith84@gmail.com>
2020-06-02 13:59:35 -0700

    Add InSpec 4.19
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- inspec/tools/chocolateyinstall.ps1
+++ inspec/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-$version = '4.18.114'
+$version = '4.19.0'
 
 $packageArgs = @{
     packageName    = 'inspec'
     unzipLocation  = $toolsDir
     fileType       = 'msi'
     url64bit       = "https://packages.chef.io/files/stable/inspec/$version/windows/2016/inspec-$version-1-x64.msi"
-    checksum64     = 'b4c91d697ac219ff5255a3648827bb6c17608b6cb2b8b0aee03d57b9bef43fd3'
+    checksum64     = 'f4c5e05d63e794edce5155128055965237121d2787b93290884bd9e4ec7e780a'
     checksumType64 = 'sha256'
     silentArgs     = "/qn /quiet /norestart"
     validExitCodes = @(0, 3010)
