e9820c1ee79f627a9bd7c7581a7cd41941f83448
Tim Smith <tsmith84@gmail.com>
2020-06-02 13:58:46 -0700

    Add DK 4.8.23
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chefdk/tools/chocolateyinstall.ps1
+++ chefdk/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '4.7.73'
+$version = '4.8.23'
 $url = "https://packages.chef.io/files/stable/chefdk/$version/windows/2012r2/chefdk-$version-1-x86.msi"
 $url64 = "https://packages.chef.io/files/stable/chefdk/$version/windows/2012r2/chefdk-$version-1-x64.msi"
 
@@ -8,9 +8,9 @@ $packageArgs = @{
     fileType       = 'MSI'
     url            = $url
     url64bit       = $url64
-    checksum       = '697c19bac7b6258d5a229a08029062720d46b9b8dcc5194c6c98c63f96d07588'
+    checksum       = '2034e7144d97c70f8739ab936bcf2e780c2a31b815d0e502c6a80e6ae11eef5f'
     checksumType   = 'sha256'
-    checksum64     = '8843852999b4b3bd427e5d061611c188e1913b062e7372c747588e5c4d535d34'
+    checksum64     = 'bd092de8f1c42f797fa20813c9a8b5ede7886386d4aefb6fe5403cb173b3b598'
     checksumType64 = 'sha256'
     silentArgs     = "/qn /quiet /norestart"
     validExitCodes = @(0, 3010)
