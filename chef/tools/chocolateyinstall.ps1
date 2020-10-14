a7d9ea64d44cdc91e962d1c5a694d7a46e57e95d
Tim Smith <tsmith84@gmail.com>
2020-10-14 16:34:20 -0700

    Update Chef to 16.5.57
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chef/tools/chocolateyinstall.ps1
+++ chef/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '16.4.41'
+$version = '16.5.57'
 
 $packageArgs = @{
     packageName    = 'chef-client'
@@ -7,9 +7,9 @@ $packageArgs = @{
     url64          = "https://packages.chef.io/files/stable/chef/$version/windows/2012r2/chef-client-$version-1-x64.msi"
     silentArgs     = '/quiet'
     validExitCodes = @(0)
-    checksum       = '08faab445bb868393be551e326757cac35a3a61202e814c84403ae55f7437d0a'
+    checksum       = 'b6d559ca63536ca8ca5a8a615ee486eeab7ab472ea6963241fe3269419f621d9'
     checksumType   = 'sha256'
-    checksum64     = '38f74615658a24cee4879a1004a7d1a56f9248a6821ef90e4f693a24d903b25d'
+    checksum64     = '5d7622cf586885ac1ab6f416d8f09424472dd9b9231f58655b5d110670c77e64'
     checksumType64 = 'sha256'
 }
 
