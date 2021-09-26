044a2975d41a4d508eac3022f6ac2530226de8df
Tim Smith <tsmith@chef.io>
2021-09-26 10:29:11 -0700

    Update to 21.9.613
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chef-workstation/tools/chocolateyinstall.ps1
+++ chef-workstation/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '0.17.5'
+$version = '21.9.613'
 $url64 = "https://packages.chef.io/files/stable/chef-workstation/$version/windows/2016/chef-workstation-$version-1-x64.msi"
 
 $packageArgs = @{
@@ -6,7 +6,7 @@ $packageArgs = @{
     unzipLocation  = $toolsDir
     fileType       = 'MSI'
     url64bit       = $url64
-    checksum64     = 'cb59b4721c176a64c211bc3a0f5accfdd06bd6d26b816d0406b2c82464ab4555'
+    checksum64     = '325456fff7db6db34c00d5d679e885aa04cef8adf4db2ebfb0c20037e492e590'
     checksumType64 = 'sha256'
     silentArgs     = "/qn /quiet /norestart"
     validExitCodes = @(0, 3010)
