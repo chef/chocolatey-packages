9f7cec1fe9960e1cec017a7c301b24b831595d48
Tim Smith <tsmith@chef.io>
2021-09-25 19:56:05 -0700

    Update chef to 17.2
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- chef/tools/chocolateyinstall.ps1
+++ chef/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '17.1.35'
+$version = '17.2.29'
 
 $packageArgs = @{
     packageName    = 'chef-client'
@@ -7,9 +7,9 @@ $packageArgs = @{
     url64          = "https://packages.chef.io/files/stable/chef/$version/windows/2012r2/chef-client-$version-1-x64.msi"
     silentArgs     = '/quiet'
     validExitCodes = @(0)
-    checksum       = '4544b8b9af64259379cd5c67581a82dbef83f44c46072d358df76a87cd606419'
+    checksum       = '4bcb7c56d10e19e7d88fa1258cc55eb37b690aaaf7edc3b6ccc92b472b8c078a'
     checksumType   = 'sha256'
-    checksum64     = 'afaa51e1dff887930962492ffc921c0df28b3c020aab1f8f2730ba23429297b6'
+    checksum64     = 'ee028f6e776a8c0df80e5614bfbab2d1ac75455c5b73566db5e200c4f05f672d'
     checksumType64 = 'sha256'
 }
 
