048e4fe63dbd468642a36f1196101f89fa0de305
John <john.mccrae@progress.com>
2023-01-05 10:24:01 -0800

    Updated for Chef 18.1.0
    Signed-off-by: John <john.mccrae@progress.com>

--- chef/tools/chocolateyinstall.ps1
+++ chef/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '18.0.185'
+$version = '18.1.0'
 
 $packageArgs = @{
     packageName    = 'chef-client'
@@ -6,7 +6,7 @@ $packageArgs = @{
     url64          = "https://packages.chef.io/files/stable/chef/$version/windows/2012r2/chef-client-$version-1-x64.msi"
     silentArgs     = '/quiet'
     validExitCodes = @(0)
-    checksum64     = '189525427A7A309B7FD37DAC52E43D0B892178EBE60F880C146E7A6D0D946B5B'
+    checksum64     = 'C2B8D8833D7D90838C56D258F9A33B2E1B0D71C90FC2B54DD5CD3E7A34C8868C'
     checksumType64 = 'sha256'
 }
 
