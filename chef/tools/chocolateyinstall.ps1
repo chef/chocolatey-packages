f3ff3ffe8b7edac00ee3da0871cd5ac199c41acd
John McCrae <john.mccrae@progress.com>
2024-01-31 08:41:41 -0800

    Updating Chocolatey for 18.4.2 release
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- chef/tools/chocolateyinstall.ps1
+++ chef/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$version = '17.10.95'
+$version = '18.4.2'
 
 $packageArgs = @{
     packageName    = 'chef-client'
@@ -6,7 +6,7 @@ $packageArgs = @{
     url64          = "https://packages.chef.io/files/stable/chef/$version/windows/2012r2/chef-client-$version-1-x64.msi"
     silentArgs     = '/quiet'
     validExitCodes = @(0)
-    checksum64     = 'f1e05ca3df288dac185cdf4ae5aa36ee8bbb6b54d772eac2feed99c9d6518ec4'
+    checksum64     = 'F676683A4DC9CA17D142710964D773FE62E59307E6F19D2D65CB92DD51C86542'
     checksumType64 = 'sha256'
 }
 
