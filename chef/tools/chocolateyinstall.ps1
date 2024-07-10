352db47e5764092b5f043ebbd07f1b40d3613382
Thomas Powell <powell@progress.com>
2024-07-10 19:59:43 -0400

    18.5.0 checksum and version
    Signed-off-by: Thomas Powell <powell@progress.com>

--- chef/tools/chocolateyinstall.ps1
+++ chef/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
     url64          = "https://packages.chef.io/files/stable/chef/$version/windows/2012r2/chef-client-$version-1-x64.msi"
     silentArgs     = '/quiet'
     validExitCodes = @(0)
-    checksum64     = '939a12b17733741799778485463aaae690a4d2512cc2c9f78b812e010640675b'
+    checksum64     = 'e9261944c6fd8ed7218d1909d280ae183d1afb87ce76b9ad033453b34b65fb52'
     checksumType64 = 'sha256'
 }
 
