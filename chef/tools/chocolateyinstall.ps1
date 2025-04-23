c0850bf898b6a0365872ecaf9a629769d36ba94b
Thomas Powell <powell@progress.com>
2025-04-23 12:59:42 -0400

    18.7.6 update
    Signed-off-by: Thomas Powell <powell@progress.com>

--- chef/tools/chocolateyinstall.ps1
+++ chef/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
-$version = '17.10.163'
+$version = '18.7.6'
 
 $packageArgs = @{
     packageName    = 'chef-client'
     fileType       = 'msi'
-    url64          = "https://packages.chef.io/files/stable/chef/$version/windows/2012r2/chef-client-$version-1-x64.msi"
+    url64          = "https://packages.chef.io/files/stable/chef/$version/windows/2016/chef-client-$version-1-x64.msi"
     silentArgs     = '/quiet'
     validExitCodes = @(0)
-    checksum64     = '64D696934F92C55D1045FC85DBA4456232FBFE13FC84F9929F536E89BEA707B2'
+    checksum64     = 'e6e62795451d7e7889c375f9036c9c33eeacc4d4fb7d32ae0feda681cd0373ab'
     checksumType64 = 'sha256'
 }
 
